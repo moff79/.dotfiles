7dca1ccc93b92b18ba0b1ad5af0ccc46db872c7d
Mohamed Fathy <mofnet@hotmail.com>
2021-03-25 07:31:18 +0200

    dwm fresh

--- dwm/suckless/dwm-6.2/config.def.h
+++ dwm/suckless/dwm-6.2/config.def.h
@@ -63,6 +63,7 @@ static const Rule rules[] = {
 	{ "xdman-Main", NULL, NULL,	0,	1,	-1},
 	{ "Subl", NULL, NULL,	0,	1,	-1},
 	{ "terminal.exe", NULL, NULL,	0,	1,	-1},
+	{ "Atom", NULL, NULL,   0,      1,      -1},
 };
 
 /* layout(s) */
